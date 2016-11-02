ee838b6f0a9efbadd532ad1571177a6f6c6c161a
ohnoahshark <mosturnbull@gmail.com>
2016-11-02 12:21:44 +0000

    added some siikkkk files

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <ifstream>
+
+using namespace std;
+
+private string* read_input(string filename) {
+	
+	return lines[0];
+}
+
+int main() {
+
+	return 0;
+}
